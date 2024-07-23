643f6425a11426bb2dfab56e9144b10197f5035d
Mohammad Aslam <aslam071595@gmail.com>
2024-07-23 15:12:56 +0530

    React router setup implimented in app.js

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from "./components/HomePage";
+import RegisterationPage from "./components/RegisterationPage";
+import LoginPage from "./components/LoginPage";
+import TaskBoard from "./components/TaskBoard";
+import SecuredRoute from "./components/SecuredRoute";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/board" element={<SecuredRoute><TaskBoard /></SecuredRoute>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ task-manager/src/components/SecuredRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SecuredRoute = () => {
+  return (
+    <div>SecuredRoute</div>
+  )
+}
+
+export default SecuredRoute
